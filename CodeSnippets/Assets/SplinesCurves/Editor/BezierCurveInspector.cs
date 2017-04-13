60984bf383fb6768f67f2b7ec5b11607827223f5
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2017-04-13 17:49:45 +0100

    Added SplineCurves

---
+++ CodeSnippets/Assets/SplinesCurves/Editor/BezierCurveInspector.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UtilityCurves
+{
+    [CustomEditor(typeof(BezierCurve))]
+    public class BezierCurveInspector : Editor
+    {
+        private BezierCurve m_Curve;
+        private Transform m_HandleTransform;
+        private Quaternion m_HandleRotation;
+
+        private const int m_LineSteps = 10;
+        private const float m_DirectionScale = 0.5f;
+
+
+        private void OnSceneGUI()
+        {
+            m_Curve = target as BezierCurve;
+            m_HandleTransform = m_Curve.transform;
+            m_HandleRotation = Tools.pivotRotation == PivotRotation.Local ?
+                m_HandleTransform.rotation : Quaternion.identity;
+
+            Vector3 p0 = ShowPoint(0);
+            Vector3 p1 = ShowPoint(1);
+            Vector3 p2 = ShowPoint(2);
+            Vector3 p3 = ShowPoint(3);
+
+            Handles.color = Color.grey;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p1, p2);
+            Handles.DrawLine(p2, p3);
+
+
+            ShowDirections();
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+
+
+
+            /*Handles.color = Color.white;
+            Vector3 lineStart = m_Curve.GetPoint(0f);
+
+            Handles.color = Color.green;
+            Handles.DrawLine(lineStart, lineStart + m_Curve.GetDirection(0f));
+            for (int i = 1; i <= m_LineSteps; i++)
+            {
+                float tParam = (i / (float)m_LineSteps);
+
+                Vector3 lineEnd = m_Curve.GetPoint(tParam);
+
+                Handles.color = Color.white;
+                Handles.DrawLine(lineStart, lineEnd);
+
+                Handles.color = Color.green;
+                Handles.DrawLine(lineEnd, lineEnd + m_Curve.GetDirection(tParam));
+
+                lineStart = lineEnd;
+            }*/
+        }
+
+        private Vector3 ShowPoint(int index)
+        {
+            Vector3 point = m_HandleTransform.TransformPoint(m_Curve.Points[index]);
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, m_HandleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(m_Curve, "Move Point");
+                EditorUtility.SetDirty(m_Curve);
+                m_Curve.Points[index] = m_HandleTransform.InverseTransformPoint(point);
+            }
+            return point;
+        }
+
+        private void ShowDirections()
+        {
+            Handles.color = Color.green;
+            Vector3 point = m_Curve.GetPoint(0f);
+            Handles.DrawLine(point, point + m_Curve.GetDirection(0f) * m_DirectionScale);
+            for (int i = 1; i <= m_LineSteps; i++)
+            {
+                point = m_Curve.GetPoint(i / (float) m_LineSteps);
+                Handles.DrawLine(point, point + m_Curve.GetDirection(i / (float) m_LineSteps) * m_DirectionScale);
+            }
+        }
+
+    }
+}
---
+++ CodeSnippets/Assets/SplinesCurves/Scripts/SplineWalker.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UtilityCurves
+{
+    public class SplineWalker : MonoBehaviour
+    {
+        [SerializeField] private BezierSpline m_Spline;
+
+        [SerializeField]
+        private float m_Duration;
+        
+        private float m_Progress;
+
+        [SerializeField]
+        private bool m_LookForward;
+
+        public enum SplineWalkerMode
+        {
+            Once,
+            Loop,
+            PingPong
+        }
+
+        [SerializeField]
+        private SplineWalkerMode m_Mode;
+        private bool m_GoingForward = true;
+
+        private void Update()
+        {
+            if (m_GoingForward)
+            {
+                m_Progress += Time.deltaTime / m_Duration;
+                if (m_Progress > 1.0f)
+                {
+                    if (m_Mode == SplineWalkerMode.Once)
+                    {
+                        m_Progress = 1.0f;
+                    }
+                    else if (m_Mode == SplineWalkerMode.Loop)
+                    {
+                        m_Progress -= 1.0f;
+                    }
+                    else
+                    {
+                        m_Progress = 2.0f - m_Progress;
+                        m_GoingForward = false;
+                    }
+                }
+
+            }
+            else
+            {
+                m_Progress -= Time.deltaTime / m_Duration;
+                if (m_Progress < 0.0f)
+                {
+                    m_Progress = -m_Progress;
+                    m_GoingForward = true;
+                }
+
+            }
+
+            Vector3 position = m_Spline.GetPoint(m_Progress);
+            transform.localPosition = position;
+
+            if (m_LookForward)
+            {
+                transform.LookAt(position + m_Spline.GetDirection(m_Progress));
+            }
+            
+        }
+
+    }
+}
