82604f88df7da8c9312e7f51ba95abe50215c858
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2018-03-29 12:20:44 +0100

    WIP:Pain Tool

---
+++ CodeSnippets/Assets/12.PaintTool/TessellationSnowExample/DrawMouse.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawMouse : MonoBehaviour
+{
+    public Camera _camera;
+    public Shader _drawShader;
+
+    private RenderTexture _splatmap;
+    private Material _snowMaterial;
+    private Material _drawMaterial;
+
+    [Range(1,500)]
+    public float _brushSize;
+    [Range(0, 1)]
+    public float _brushStrength;
+
+    private RaycastHit _hit;
+
+	void Start ()
+    {
+        _drawMaterial = new Material(_drawShader);
+        _drawMaterial.SetVector("_Color", Color.red);
+
+        _snowMaterial = GetComponent<MeshRenderer>().material;
+        _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        _snowMaterial.SetTexture("_Splat", _splatmap);
+    }
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            
+
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out _hit))
+            {
+                //Debug.Log("TextureCoord (" + _hit.textureCoord.x + "," + _hit.textureCoord.y + ")");
+               // Debug.Log("Input.mousePosition (" + Input.mousePosition.x + "," + Input.mousePosition.y + ")");
+
+                // Send coordinates to the material where the input was donw
+                _drawMaterial.SetVector("_Coordinate", new Vector4(_hit.textureCoord.x, _hit.textureCoord.y));
+                _drawMaterial.SetFloat("_Strength", _brushStrength);
+                _drawMaterial.SetFloat("_Size", _brushSize);
+
+                RenderTexture temp = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(_splatmap, temp);
+                // use draw material only when we click on a certain position on a mesh
+                Graphics.Blit(temp, _splatmap, _drawMaterial);
+
+                // Release temporary render texture
+                RenderTexture.ReleaseTemporary(temp);
+            }
+        }
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 256, 256), _splatmap, ScaleMode.ScaleToFit, false, 1);
+    }
+}
---
+++ CodeSnippets/Assets/12.PaintTool/TessellationSnowExample/DrawTracks.shader
@@ -0,0 +1,75 @@
+﻿Shader "Unlit/DrawTracks"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Coordinate("_Coordinate", Vector) = (0,0,0,0)
+		_Color("Draw Color", Color) = (1,0,0,0)
+		_Size("Size", Range(1,500)) = 1
+		_Strength("Strength", Range(0,1)) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed4 _Coordinate;
+			fixed4 _Color;
+			half _Size;
+			half _Strength;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// We want to spawn a brush where we have clicked on our mesh once we click the mesh
+				// this shader is being drawn onto the render texture
+				// The uv coordinates are always between 0 and 1
+				// Anything done in the frag program will be calcluated on each pixel 
+				// If we do a distance check between teh current pixel and the coordinates we know which pixel is at 
+				// the same place as teh coordinates
+				// As we need the color we need to invert the outcome of that operation
+				// Saturate is the same as clamp to 1 to 0 in c#
+
+				// _Size Is for the brush size, we have to invert the value, to get the less size the smaller the brush
+				float draw = pow (saturate(1 - distance(i.uv, _Coordinate.xy)),(500 / _Size));
+				// Strenght of the brush
+				fixed4 drawcol = _Color * (draw * _Strength);
+				return saturate(col + drawcol);
+
+			}
+			ENDCG
+		}
+	}
+}
