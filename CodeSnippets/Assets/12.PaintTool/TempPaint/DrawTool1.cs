f1350aa259900aeb081cc32a90ab177da2a395f1
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2018-03-29 17:12:39 +0100

    Update Paint tool

--- CodeSnippets/Assets/12.PaintTool/TempPaint/DrawTool1.cs
+++ CodeSnippets/Assets/12.PaintTool/TempPaint/DrawTool1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Utility.DrawTool
 {
@@ -22,23 +23,35 @@ namespace Utility.DrawTool
 
         [SerializeField] private Vector2Int m_IntTextCoord;
         [SerializeField] private Vector2 m_TextCoord;
+
+
+        [SerializeField] private Image m_PreviewImage;
+        [SerializeField] private Texture2D m_Picture;
+
         void Start()
-        {            
+        {
+            float ySize = Camera.main.orthographicSize * 2.0f;
+            float xSize = ySize * Screen.width / Screen.height;
+            //m_Plane.transform.localScale = new Vector3(xSize/10, xSize/10, 1);
+
 
             m_PlaneMaterial = m_Plane.GetComponent<MeshRenderer>().material;
 
-            
-            Texture2D tempText = m_PlaneMaterial.GetTexture("_MainTex") as Texture2D;
-            tex = new Texture2D(tempText.width, tempText.height);
 
-            // Set first texture
-            for (int x = 0; x < tempText.width; x++)
+            Texture2D tempText = m_PlaneMaterial.GetTexture("_MainTex") as Texture2D;
+            // tex = new Texture2D(tempText.width, tempText.height);
+
+            // Set the texture
+            tex = new Texture2D(m_Picture.width, m_Picture.height);
+
+            for (int x = 0; x < tex.width; x++)
             {
-                for (int y = 0; y < tempText.height; y++)
+                for (int y = 0; y < tex.height; y++)
                 {
-                    Color c = tempText.GetPixel(x, y);
+                    Color c = m_Picture.GetPixel(x, y);
 
                     tex.SetPixel(x,y, c);
+                    tempText.SetPixel(x, y, c);
 
                 }
             }
@@ -47,6 +60,22 @@ namespace Utility.DrawTool
             //What happens to points drawn outside the texture boundaries 
             // depends on whether the texture is set to clamp or repeat: clamp means the circle won't draw past the edges, and repeat makes the circle start over from the other side (this behavior is inherent to SetPixel).
             m_PlaneMaterial.SetTexture("_MainTex", tex);
+
+            Rect rec = new Rect(0, 0, tex.width, tex.height);
+            Sprite spr = Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f), 100);
+            m_PreviewImage.sprite = spr;
+
+            float ratio = (tex.width / tex.height);
+
+            m_PreviewImage.preserveAspect = true;
+
+            
+
+
+            /*if (m_AspectRatioFitter != null)
+            {
+                m_AspectRatioFitter.aspectRatio = ratio;
+            }*/
         }
 
         void Update()
@@ -56,57 +85,63 @@ namespace Utility.DrawTool
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
                 if (Input.GetKey(KeyCode.Mouse0))
-                {
+                {                    
                     if (Physics.Raycast(ray, out hit, Mathf.Infinity))
                     {
-                        // Find the u,v coordinate of the Texture
-                        Vector2 uv;
-                        uv.x = (hit.point.x - hit.collider.bounds.min.x) / hit.collider.bounds.size.x;
-                        uv.y = (hit.point.y - hit.collider.bounds.min.y) / hit.collider.bounds.size.y;
+                        Debug.Log(" HIT " + hit.collider.tag);
+                        if (hit.collider.tag == "Picture")
+                        {
+                            
+
+                            // Find the u,v coordinate of the Texture
+                            Vector2 uv;
+                            uv.x = (hit.point.x - hit.collider.bounds.min.x) / hit.collider.bounds.size.x;
+                            uv.y = (hit.point.y - hit.collider.bounds.min.y) / hit.collider.bounds.size.y;
 
 
-                       // m_TextCoord = hit.textureCoord;
+                            // m_TextCoord = hit.textureCoord;
 
-                        // Set Color in each pixel
-                        int xCoord = (int)(-uv.x * tex.width);
-                        int yCoord = (int)(uv.y * tex.height);
+                            // Set Color in each pixel
+                            int xCoord = (int)(-uv.x * tex.width);
+                            int yCoord = (int)(uv.y * tex.height);
 
-                        //int xCoord = (int)(hit.textureCoord.x);
-                        //int yCoord = (int)(hit.textureCoord.y);
-                        //m_IntTextCoord = new Vector2Int(xCoord, yCoord);
-
-
-                        // Paint pixel
-                        Circle(tex, xCoord, yCoord, m_BrushSize, m_BrushColor);
-                        // Pixel in coords
-                        /*tex.SetPixel(xCoord, yCoord, m_BrushColor);
-
-                        tex.SetPixel(xCoord - 1, yCoord - 1, m_BrushColor);  // Left Up
-                        tex.SetPixel(xCoord + 1, yCoord - 1, m_BrushColor); // Right Up
-                        tex.SetPixel(xCoord, yCoord - 1, m_BrushColor); // Up
-
-                        tex.SetPixel(xCoord + 1, yCoord, m_BrushColor); // Right
-                        tex.SetPixel(xCoord + 1, yCoord + 1, m_BrushColor); // Down Right
-                        tex.SetPixel(xCoord, yCoord + 1, m_BrushColor); // Down   
-                        tex.SetPixel(xCoord - 1, yCoord + 1, m_BrushColor); // Down Left
-
-                        tex.SetPixel(xCoord - 1, yCoord, m_BrushColor); // Right
-
-                        tex.Apply();*/
+                            //int xCoord = (int)(hit.textureCoord.x);
+                            //int yCoord = (int)(hit.textureCoord.y);
+                            //m_IntTextCoord = new Vector2Int(xCoord, yCoord);
 
 
+                            // Paint pixel
+                            Circle(tex, xCoord, yCoord, m_BrushSize, m_BrushColor);
+                            // Pixel in coords
+                            /*tex.SetPixel(xCoord, yCoord, m_BrushColor);
 
-                        //Circle(tex, xCoord, yCoord, 1, m_BrushColor);
-                        /*tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height), m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height) + 1, m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height), m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height) - 1, m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height), m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height) + 1, m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height) - 1, m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height) + 1, m_BrushColor);
-                        tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height) - 1, m_BrushColor);
-                        tex.Apply();*/
+                            tex.SetPixel(xCoord - 1, yCoord - 1, m_BrushColor);  // Left Up
+                            tex.SetPixel(xCoord + 1, yCoord - 1, m_BrushColor); // Right Up
+                            tex.SetPixel(xCoord, yCoord - 1, m_BrushColor); // Up
+
+                            tex.SetPixel(xCoord + 1, yCoord, m_BrushColor); // Right
+                            tex.SetPixel(xCoord + 1, yCoord + 1, m_BrushColor); // Down Right
+                            tex.SetPixel(xCoord, yCoord + 1, m_BrushColor); // Down   
+                            tex.SetPixel(xCoord - 1, yCoord + 1, m_BrushColor); // Down Left
+
+                            tex.SetPixel(xCoord - 1, yCoord, m_BrushColor); // Right
+
+                            tex.Apply();*/
+
+
+
+                            //Circle(tex, xCoord, yCoord, 1, m_BrushColor);
+                            /*tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height), m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height) + 1, m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height), m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width), (int)(uv.y * tex.height) - 1, m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height), m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height) + 1, m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height) - 1, m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) - 1, (int)(uv.y * tex.height) + 1, m_BrushColor);
+                            tex.SetPixel((int)(-uv.x * tex.width) + 1, (int)(uv.y * tex.height) - 1, m_BrushColor);
+                            tex.Apply();*/
+                        }
                     }
                 }
                 /*if (Input.GetKey(KeyCode.Mouse1))
@@ -142,11 +177,26 @@ namespace Utility.DrawTool
                     tempArray[py * 1024 + nx] = col;
                     tempArray[ny * 1024 + px] = col;
                     tempArray[ny * 1024 + nx] = col;*/
+                    if ((py * tex.width + px) < tempArray.Length)
+                    {
+                        tempArray[py * tex.width + px] = col;
+                    }
 
-                    tempArray[py * tex.width + px] = col;
-                    tempArray[py * tex.width + nx] = col;
-                    tempArray[ny * tex.width + px] = col;
-                    tempArray[ny * tex.width + nx] = col;
+                    if ((py * tex.width + nx) < tempArray.Length)
+                    {
+                        tempArray[py * tex.width + nx] = col;
+                    }
+
+                    if ((ny * tex.width + px) < tempArray.Length)
+                    {
+                        tempArray[ny * tex.width + px] = col;
+                    }
+
+                    if ((ny * tex.width + nx) < tempArray.Length)
+                    {
+                        tempArray[ny * tex.width + nx] = col;
+                    }
+                    
                 }
             }
             in_tex.SetPixels32(tempArray);
---
+++ CodeSnippets/Assets/12.PaintTool/TempPaint/ImagePointer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ImagePointer : MonoBehaviour, IPointerClickHandler
+{
+
+    [SerializeField] private Texture2D texture;
+
+    private void Start()
+    {
+        texture = GetComponent<Image>().sprite.texture;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector2 localCursor;
+        var rect1 = GetComponent<RectTransform>();
+        var pos1 = eventData.position;
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rect1, pos1,
+            null, out localCursor))
+            return;
+
+        int xpos = (int)(localCursor.x);
+        int ypos = (int)(localCursor.y);
+
+        if (xpos < 0) xpos = xpos + (int)rect1.rect.width / 2;
+        else xpos += (int)rect1.rect.width / 2;
+
+        if (ypos > 0) ypos = ypos + (int)rect1.rect.height / 2;
+        else ypos += (int)rect1.rect.height / 2;
+
+        Debug.Log("Correct Cursor Pos: " + xpos + " " + ypos);
+
+        int xCoord = (xpos * texture.width);
+        int yCoord = (ypos * texture.height);
+
+        Debug.Log("Coords: " + xCoord + " " + yCoord);
+
+        texture.SetPixel(xCoord, yCoord, Color.red);
+        texture.Apply();
+
+        //Vector2 position;
+        //GetPositionOnImage01(GetComponent<Image>(), localCursor, out position);
+
+        //Debug.Log("GetPositionOnImage01 Pos: " + position.x + " " + position.y);
+    }
+
+
+    // Take an image and a screenPosition. Return the ptLocationRelativeToImage01 relative to the image (where x,y between 0.0 and 1.0 are on the image, values below 0.0 or above 1.0 are outside the image).
+    public bool GetPositionOnImage01(
+        UnityEngine.UI.Image uiImageObject,
+        Vector2 screenPosition,
+        out Vector2 ptLocationRelativeToImage01)
+    {
+        ptLocationRelativeToImage01 = new Vector2();
+        RectTransform uiImageObjectRect = uiImageObject.GetComponent<RectTransform>();
+        Vector2 localCursor = new Vector2();
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            uiImageObjectRect,
+            screenPosition,
+            null,
+            out localCursor))
+        {
+            Vector2 ptPivotCancelledLocation = new Vector2(localCursor.x - uiImageObjectRect.rect.x, localCursor.y - uiImageObjectRect.rect.y);
+            Vector2 ptLocationRelativeToImageInScreenCoordinates = new Vector2();
+            // How do we get the location of the image? Calculate the size of the image, then use the pivot information.
+            if (uiImageObject.preserveAspect)
+            {
+                // If we are preserving the aspect ratio of the image, then we need to do some additional calculations
+                // Figure out if the image constrained by height or by width.
+                float fImageAspectRatio = uiImageObject.sprite.rect.height / uiImageObject.sprite.rect.width;
+                float fRectAspectRatio = uiImageObjectRect.rect.height / uiImageObjectRect.rect.width;
+                Rect imageRectInLocalScreenCoordinates = new Rect();
+                if (fImageAspectRatio > fRectAspectRatio)
+                {
+                    // The image is constrained by its height.
+                    float fImageWidth = (fRectAspectRatio / fImageAspectRatio) * uiImageObjectRect.rect.width;
+                    float fExcessWidth = uiImageObjectRect.rect.width - fImageWidth;
+                    imageRectInLocalScreenCoordinates.Set(uiImageObjectRect.pivot.x * fExcessWidth, 0, uiImageObjectRect.rect.height / fImageAspectRatio, uiImageObjectRect.rect.height);
+                }
+                else
+                {
+                    // The image is constrained by its width.
+                    float fImageHeight = (fImageAspectRatio / fRectAspectRatio) * uiImageObjectRect.rect.height;
+                    float fExcessHeight = uiImageObjectRect.rect.height - fImageHeight;
+                    imageRectInLocalScreenCoordinates.Set(0, uiImageObjectRect.pivot.y * fExcessHeight, uiImageObjectRect.rect.width, fImageAspectRatio * uiImageObjectRect.rect.width);
+                }
+                ptLocationRelativeToImageInScreenCoordinates.Set(ptPivotCancelledLocation.x - imageRectInLocalScreenCoordinates.x, ptPivotCancelledLocation.y - imageRectInLocalScreenCoordinates.y);
+                ptLocationRelativeToImage01.Set(ptLocationRelativeToImageInScreenCoordinates.x / imageRectInLocalScreenCoordinates.width, ptLocationRelativeToImageInScreenCoordinates.y / imageRectInLocalScreenCoordinates.height);
+            }
+            else
+            {
+                ptLocationRelativeToImageInScreenCoordinates.Set(ptPivotCancelledLocation.x, ptPivotCancelledLocation.y);
+                ptLocationRelativeToImage01.Set(ptLocationRelativeToImageInScreenCoordinates.x / uiImageObjectRect.rect.width, ptLocationRelativeToImageInScreenCoordinates.y / uiImageObjectRect.rect.height);
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
