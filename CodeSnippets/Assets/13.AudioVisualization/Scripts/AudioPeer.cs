a22e0c599faad124457c645147b90945f61969bd
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2018-04-06 16:09:03 +0100

    Include audio visualizer

---
+++ CodeSnippets/Assets/13.AudioVisualization/Scripts/AudioPeer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+
+    public static float[] m_Samples = new float[512];
+
+	void Start ()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+	void Update ()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        // Get all the samples from the audio source
+        m_AudioSource.GetSpectrumData(m_Samples,0, FFTWindow.Blackman);
+    }
+}
