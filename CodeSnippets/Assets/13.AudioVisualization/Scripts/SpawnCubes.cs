2a17ab58e9cc078f742f2c89afe51bd10a4ab870
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2018-04-18 14:33:36 +0100

    Update Audio visualizetion

--- CodeSnippets/Assets/13.AudioVisualization/Scripts/SpawnCubes.cs
+++ CodeSnippets/Assets/13.AudioVisualization/Scripts/SpawnCubes.cs
@@ -2,44 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnCubes : MonoBehaviour
+
+namespace AudioVisualizer
 {
-    [SerializeField] private GameObject m_SamplePrefab;
-
-    private GameObject[] m_SampleCubes = new GameObject[512];
-
-    [SerializeField] private float m_MaxScale;
-
-
-	void Start ()
+    public class SpawnCubes : MonoBehaviour
     {
-        float angleStep = (360.0f / m_SampleCubes.Length);
+        [SerializeField] private GameObject m_SamplePrefab;
 
-        for (int i=0; i< m_SampleCubes.Length; i++)
+        private GameObject[] m_SampleCubes = new GameObject[512];
+
+        [SerializeField] private float m_MaxScale;
+
+
+        void Start()
         {
-            GameObject instanceSample = Instantiate(m_SamplePrefab);
-            instanceSample.transform.position = transform.position;
-            instanceSample.transform.parent = transform;
-            instanceSample.name = "SampleCube" + i;
+            float angleStep = (360.0f / m_SampleCubes.Length);
 
-            // Rotate to make a circle
-            transform.eulerAngles = new Vector3(0.0f, -angleStep * i, 0.0f);
-            instanceSample.transform.position = Vector3.forward * 30.0f;
-
-            m_SampleCubes[i] = instanceSample;
-        }
-	}
-
-    void Update()
-    {
-        if ((m_SampleCubes == null) || (AudioPeer.m_Samples == null)) return;
-        for (int i = 0; i < m_SampleCubes.Length; i++)
-        {
-            if (i < AudioPeer.m_Samples.Length)
+            for (int i = 0; i < m_SampleCubes.Length; i++)
             {
-                m_SampleCubes[i].transform.localScale = new Vector3(2.0f, (AudioPeer.m_Samples[i] * m_MaxScale) + 2.0f, 2.0f);
+                GameObject instanceSample = Instantiate(m_SamplePrefab);
+                instanceSample.transform.position = transform.position;
+                instanceSample.transform.parent = transform;
+                instanceSample.name = "SampleCube" + i;
+
+                // Rotate to make a circle
+                transform.eulerAngles = new Vector3(0.0f, -angleStep * i, 0.0f);
+                instanceSample.transform.position = Vector3.forward * 30.0f;
+
+                m_SampleCubes[i] = instanceSample;
             }
-            
+        }
+
+        void Update()
+        {
+            /*if ((m_SampleCubes == null) || (AudioPeer.m_Samples == null)) return;
+            for (int i = 0; i < m_SampleCubes.Length; i++)
+            {
+                if (i < AudioPeer.m_Samples.Length)
+                {
+                    m_SampleCubes[i].transform.localScale = new Vector3(2.0f, (AudioPeer.m_Samples[i] * m_MaxScale) + 2.0f, 2.0f);
+                }
+
+            }*/
         }
     }
 }
