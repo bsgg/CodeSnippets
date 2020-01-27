39a41a2bef35c7f78d8fa00075842490801b3b1f
bsgg <beatriz.s.g86@gmail.com>
2020-01-27 12:09:10 +0000

    AnimatedText example

---
+++ CodeSnippets/Assets/22.AnimatedText/Scripts/AnimatedText.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace SnippetsCode.AnimatedDialogText
+{
+    public class AnimatedText : MonoBehaviour
+    {
+        public delegate void AnimatedTextCompleted();
+        public static event AnimatedTextCompleted OnAnimatedTextCompleted;
+
+        [SerializeField] private Text displayedText;
+        [SerializeField] private float delayBettweenLetters = 0.1f;            
+
+        private bool updateAnimation = false;
+
+        private string textToAnimate;
+        private string currentText;
+
+        private float currentDeltaTime;
+        private int indexLetter;        
+
+        public void AnimateText(float startDelay, string text)
+        {
+            textToAnimate = text;
+            indexLetter = 0;
+            currentText = "";
+
+            displayedText.text = currentText;
+            StartCoroutine(StartAnimation(startDelay));
+        }
+
+        private IEnumerator StartAnimation(float startDelay)
+        {
+            yield return new WaitForSeconds(startDelay);
+
+            // Set first letter
+            if (indexLetter < textToAnimate.Length)
+            {
+                currentText += textToAnimate[indexLetter];
+
+                displayedText.text = currentText; 
+            }
+
+            currentDeltaTime = 0.0f;
+            updateAnimation = true;
+        }
+       
+        void Update()
+        {
+            if (!updateAnimation) return;
+
+            currentDeltaTime += Time.deltaTime;
+            if (currentDeltaTime >= delayBettweenLetters)
+            {
+                indexLetter += 1;
+                currentDeltaTime = 0.0f;
+                if (indexLetter < textToAnimate.Length)
+                {
+                    currentText += textToAnimate[indexLetter];
+
+                    displayedText.text = currentText;
+                }else
+                {
+                    displayedText.text = textToAnimate;
+
+                    if (OnAnimatedTextCompleted != null)
+                    {
+                        OnAnimatedTextCompleted();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodeSnippets/Assets/22.AnimatedText/Scripts/AnimatedTextTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnippetsCode.AnimatedDialogText
+{
+    public class AnimatedTextTest : MonoBehaviour
+    {
+        [SerializeField] private AnimatedText animatedText;
+
+        [SerializeField] private string TextToAnimate;
+
+        void Start()
+        {
+            animatedText.AnimateText(0.40f, TextToAnimate);
+            AnimatedText.OnAnimatedTextCompleted += AnimatedText_OnAnimatedTextCompleted;
+        }
+
+        private void AnimatedText_OnAnimatedTextCompleted()
+        {
+            AnimatedText.OnAnimatedTextCompleted -= AnimatedText_OnAnimatedTextCompleted;
+
+            Debug.Log("AnimatedText_OnAnimatedTextCompleted");
+        }
+        
+    }
+}
